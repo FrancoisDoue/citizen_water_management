4c7a10b84d1d20798f6248e40ffee1104a060335
Florian <floriandelafosse@gmail.com>
2024-10-02 16:51:43 +0200

    user consommation  eau de la maison pour le dernier mois

--- water_management_backend/src/main/java/com/citizen/water_management/repository/ConsumptionDetailRepository.java
+++ water_management_backend/src/main/java/com/citizen/water_management/repository/ConsumptionDetailRepository.java
@@ -1,9 +1,12 @@
 package com.citizen.water_management.repository;
 
 import com.citizen.water_management.entity.ConsumptionDetail;
+import com.citizen.water_management.entity.WaterSupply;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ConsumptionDetailRepository extends JpaRepository<ConsumptionDetail, Long> {
+
+     ConsumptionDetail findFirstByWaterSupplyOrderByEndDateDesc(WaterSupply waterSupply);
 }
--- water_management_backend/src/main/java/com/citizen/water_management/services/LocationService.java
+++ water_management_backend/src/main/java/com/citizen/water_management/services/LocationService.java
@@ -1,6 +1,7 @@
 package com.citizen.water_management.services;
 
 import com.citizen.water_management.dto.waterSupply.WaterSupplyDtoGetForHistoryOfConsumption;
+import com.citizen.water_management.entity.ConsumptionDetail;
 import com.citizen.water_management.entity.WaterSupply;
 import com.citizen.water_management.entity.account.Account;
 import com.citizen.water_management.entity.location.Home;
@@ -11,6 +12,7 @@ import com.citizen.water_management.repository.WaterSupplyRepository;
 import com.citizen.water_management.repository.account.AccountRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -36,14 +38,33 @@ public class LocationService {
         this.homeRepository = homeRepository;
     }
 
-    public List<WaterSupplyDtoGetForHistoryOfConsumption> getHistoryOfConsumptions(int idUser){
+    //Fonction pour avoir la liste des arrivées d'eau liées à un user
+    private List<WaterSupply> getWaterSupplyByIdUser(int idUser) {
         Optional <Account> account = accountRepository.findById(idUser);
         if (account.isEmpty()) throw new IllegalArgumentException("There is no account with id " + idUser);
 
         Optional <Home> home = homeRepository.findFirstByAccounts(List.of(account.get()));
         if (home.isEmpty()) throw new IllegalArgumentException("User with id " + idUser + " have no home");
 
-        List<WaterSupply> waterSupplies = home.get().getWaterSupply();
+        return home.get().getWaterSupply();
+    }
+
+    public List<WaterSupplyDtoGetForHistoryOfConsumption> getHistoryOfConsumptions(int idUser){
+        List<WaterSupply> waterSupplies = getWaterSupplyByIdUser(idUser);
+        return waterSupplies.stream().map(WaterSupplyDtoGetForHistoryOfConsumption::new).toList();
+    }
+
+    public List<WaterSupplyDtoGetForHistoryOfConsumption> getLastConsumptions(int idUser){
+
+        List<WaterSupply> waterSupplies = getWaterSupplyByIdUser(idUser);
+
+
+        for (WaterSupply waterSupply : waterSupplies){
+            waterSupply.getConsumptionDetailList().clear();
+            waterSupply.getConsumptionDetailList().add(
+                    consumptionDetailRepository.findFirstByWaterSupplyOrderByEndDateDesc(waterSupply));
+        }
+
         return waterSupplies.stream().map(WaterSupplyDtoGetForHistoryOfConsumption::new).toList();
     }
 }
